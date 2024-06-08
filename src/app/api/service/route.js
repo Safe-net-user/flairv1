6f71a67ab0e7fda7b0391612cbff287a0f62088d
Safe-net-user <146375103+Safe-net-user@users.noreply.github.com>
2024-06-08 16:27:43 +0000

    retr

--- src/app/api/service/route.js
+++ src/app/api/service/route.js
@@ -1,35 +1,59 @@
-const { prisma } = require("@/lib/prisma");
+import { prisma } from "@/lib/prisma";
 
-// Point de terminaison pour la méthode GET
-export async function GET(req, res) {
+// Fonction pour afficher tous les services
+export default function AllServices({ services }) {
+  return (
+    <ul>
+      {services.map(service => (
+        <div key={service.id} style={{ border: 'solid 2px #ECECEC', padding: '25px', marginTop: '5%' }} className="flex justify-between items-start rounded">
+          <div className="flex flex-col justify-start items-start" style={{ width: '70%' }}>
+            <div className='flex'>
+              <button style={{ background: '#ECECEC' }} className="text-lg rounded py-2 px-4">{service.category}</button>
+              {service.domicile === true && (
+                <button style={{ color: '#2DB742', background: '#ABEAB5' }}>Service à domicile</button>
+              )}
+            </div>
+            <br />
+            <h1>{service.title}</h1>
+            <br />
+            <p>{service.description}</p>
+          </div>
+          <div className="flex flex-col items-end justify-between p-4">
+            <h1 style={{ fontSize: '250%' }} className="font-bold">{service.price} €</h1>
+            <span style={{ color: '#EAEAEA' }}>Durée {service.dureeRDV}</span>
+            <Link href={'/dashboard_pro/services/modifierService'}>
+              <button style={{ width: '150px' }} className="cursor-pointer bg-black text-lg text-white rounded py-3 px-6 mt-4">Modifier</button>
+            </Link>
+            <br />
+            <Link href={'/dashboard_pro/services/modifierService'}>
+              <button style={{ background: '#EAEAEA', width: '150px' }} className="cursor-pointer text-lg  text-black rounded py-3 px-6 mt-4">Supprimer</button>
+            </Link>
+          </div>
+        </div>
+      ))}
+    </ul>
+  );
+}
+
+// Fonction pour récupérer les services à afficher
+export async function getServerSideProps() {
   try {
-    // Récupération des services depuis la base de données avec Prisma
-    const services = await prisma.service.findMany({
-      select: {
-        title: true,
-        description: true,
-        category: true,
-        price: true,
-        domicile: true,
-        image: true,
-        dureeRDV: true,
+    // Récupération de tous les services depuis la base de données avec Prisma
+    const services = await prisma.service.findMany();
+
+    // Retourner les services en tant que props
+    return {
+      props: {
+        services,
       },
-    });
-
-    console.log('Services récupérés depuis la base de données:', services);
-
-    // Définition de l'en-tête Content-Type
-    res.setHeader('Content-Type', 'application/json');
-
-    // Envoi des données JSON
-    res.end(JSON.stringify(services));
+    };
   } catch (error) {
     console.error('Erreur lors de la récupération des services:', error);
-    
-    // Définition du statut de réponse
-    res.statusCode = 500;
-
-    // Envoi des données d'erreur JSON
-    res.end(JSON.stringify({ error: 'Erreur lors de la récupération des services' }));
+    // Retourner une liste vide en cas d'erreur
+    return {
+      props: {
+        services: [],
+      },
+    };
   }
 }
--- src/components/dashboard/professional/Service/index.tsx
+++ src/components/dashboard/professional/Service/index.tsx
@@ -1,11 +1,12 @@
-'use client';
+'use client'
 import { TabsContent } from '@/components/ui/tabs';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 import Link from 'next/link';
+import AllServices from '@/app/api/service/route';
 
 interface Service {
   id: string;
@@ -19,69 +20,11 @@ interface Service {
 }
 
 export default function ServicesTab() {
-  const [services, setServices] = useState<Service[]>([]);
   const [sortOption, setSortOption] = useState<string>('');
-
-  const fetchData = async () => {
-    try {
-      const response = await fetch('/api/service', {
-        headers: {
-          'Cache-Control': 'no-cache, no-store, must-revalidate',
-          'Pragma': 'no-cache',
-          'Expires': '0',
-        }
-      });
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      const data = await response.json();
-      setServices(data);
-    } catch (error) {
-      console.error('Error fetching services:', error);
-    }
-  };
-
-  useEffect(() => {
-    fetchData();
-    const intervalId = setInterval(fetchData, 6000); 
-
-    return () => clearInterval(intervalId); 
-  }, []);
-
 
   const handleSortChange = (event: SelectChangeEvent<string>) => {
     const selectedOption = event.target.value;
     setSortOption(selectedOption);
-    
-    let sortedServices = [...services];
-  
-    switch (selectedOption) {
-      case 'recent':
-        // Ajustez le tri selon vos besoins
-        break;
-      case 'a-z':
-        sortedServices.sort((a, b) => {
-          // Compare les titres des services
-          return a.title.localeCompare(b.title);
-        });
-        break;
-      case 'categorie':
-        sortedServices.sort((a, b) => {
-          // Compare les catégories des services
-          return a.category.localeCompare(b.category);
-        });
-        break;
-      case 'prix':
-        sortedServices.sort((a, b) => {
-          // Compare les prix des services, en supposant que les prix sont des nombres
-          return parseFloat(a.price) - parseFloat(b.price);
-        });
-        break;
-      default:
-        break;
-    }
-  
-    setServices(sortedServices);
   };
   
   return (
@@ -114,40 +57,7 @@ export default function ServicesTab() {
         </Link>
       </div>
       <div>
-        {services.length > 0 ? (
-          <ul>
-           {services.map(service => (
-  <div key={service.id} style={{border:'solid 2px #ECECEC', padding:'25px', marginTop:'5%'}} className="flex justify-between items-start rounded">
-    <div className="flex flex-col justify-start items-start" style={{width:'70%'}}>
-      <div className='flex'>
-      <button style={{background:'#ECECEC'}} className="text-lg rounded py-2 px-4">{service.category}</button>
-      {service.domicile === true &&(
-        <button style={{color:'#2DB742', background:'#ABEAB5'}}>Service à domicile</button>
-      )}
-      </div>
-      <br/>
-      <h1>{service.title}</h1>
-      <br/>
-      <p>{service.description}</p>
-    </div>
-    <div className="flex flex-col items-end justify-between p-4">
-     
-      <h1 style={{fontSize:'250%'}} className="font-bold">{service.price} €</h1>
-      <span style={{ color:'#EAEAEA'}}>Durée {service.dureeRDV}</span>
-      <Link href={'/dashboard_pro/services/modifierService'}>
-        <button style={{width:'150px'}} className="cursor-pointer bg-black text-lg text-white rounded py-3 px-6 mt-4">Modifier</button>
-      </Link>
-      <br />
-      <Link href={'/dashboard_pro/services/modifierService'}>
-        <button style={{background:'#EAEAEA', width:'150px'}} className="cursor-pointer text-lg  text-black rounded py-3 px-6 mt-4">Supprimer</button>
-      </Link>
-    </div>
-  </div>
-))}
-          </ul>
-        ) : (
-          <p>Aucun service disponible.</p>
-        )}
+        <AllServices services={[]} /> 
       </div>
     </TabsContent>
   );
